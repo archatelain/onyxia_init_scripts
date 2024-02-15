dfc100f1c7ea8943edcfacce86a976df40ee9903
archatelain <98733000+archatelain@users.noreply.github.com>
2024-02-15 11:15:19 +0100

    new test

--- test_python.sh
+++ test_python.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # On enregistre tous les logs dans log.out pour pouvoir déboguer
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>log.out 2>&1
+# exec 3>&1 4>&2
+# trap 'exec 2>&4 1>&3' 0 1 2 3
+# exec 1>log.out 2>&1
 
 # Pareil pour les variables d'environnement
 env | sort > env_init.out
